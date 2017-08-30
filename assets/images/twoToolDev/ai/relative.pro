5f271df0a963fa317d65faaaad1c0368c8681463
zhengweifu_win <zhengweifu@bitmap3d.com.cn>
2017-08-30 17:45:01 +0800

    Test pro-loader

---
+++ assets/images/twoToolDev/ai/relative.pro
@@ -0,0 +1,7 @@
+[{
+	"name": "路径分割工具",
+	"imagePath": "路径分割工具.png",
+	"scriptPath": "code.jsx",
+	"bgColor": "#000000",
+	"type": "javascript"
+}]
--- js/components/Work/AIPage.js
+++ js/components/Work/AIPage.js
@@ -6,6 +6,8 @@ import CommonRenderRelative from './CommonRenderRelative';
 
 import { WEB_ROOT } from '../../config';
 
+require('../../../assets/images/twoToolDev/ai/relative.pro');
+
 export default () => {
 	return <WorkPage siderSelectedKey='6-1'  breadcrunbs={['工作', '平面工具开发', 'AI']}>
 		<CommonRenderRelative relativePathName={`${WEB_ROOT}assets/images/twoToolDev/ai/`} relativeFileName='relative.json' />
